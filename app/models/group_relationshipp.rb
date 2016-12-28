959297850559ea6a7a19f46c076682095ff73faf
meiputi <meiputi@gmail.com>
2016-12-29 00:41:03 +0800

    implement is_member_of?(group)

---
+++ app/models/group_relationshipp.rb
@@ -0,0 +1,4 @@
+class GroupRelationshipp < ApplicationRecord
+  belongs_to :group
+  belongs_to :user
+end
---
+++ db/migrate/20161228162103_create_group_relationshipps.rb
@@ -0,0 +1,10 @@
+class CreateGroupRelationshipps < ActiveRecord::Migration[5.0]
+  def change
+    create_table :group_relationshipps do |t|
+      t.integer :group_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
